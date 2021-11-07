5e41155bad9b38e8806738072821e7fcc35e7f40
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-07 19:18:58 +0100

    Change order of static files settings in settings.py

--- anime_art/settings.py
+++ anime_art/settings.py
@@ -154,11 +154,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-
+# Extra places for collectstatic to find static files.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
