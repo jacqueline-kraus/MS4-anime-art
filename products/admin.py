f33ad8b1b694cbba69ad017fa1ceffac3adf131e
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-07 15:20:29 +0100

    Add customization to admin for products and types

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Product, Type
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Type)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'type',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Type, TypeAdmin)
