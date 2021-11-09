7248b9b7f125c3b6fe5250ed49a689ac67299d34
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-09 20:48:18 +0100

    Add filter functionality to filter by type of product

--- products/views.py
+++ products/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Type
 
 def product_list(request):
 
     products = Product.objects.all()
     query = None
+    types = None
 
     if request.GET:
+        if 'type' in request.GET:
+            types = request.GET['type'].split(',')
+            products = products.filter(type__name__in=types)
+            types = Type.objects.filter(name__in=types)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,6 +28,7 @@ def product_list(request):
     context = {
         'products': products, 
         'search_term': query,
+        'current_types': types,
     }
 
     return render(request ,'products/products.html', context)
