749d2d7b4031293124bdd724bbe0846ab7299486
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-09 20:30:13 +0100

    Add seearch functionality

--- products/views.py
+++ products/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 def product_list(request):
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter your search criteria")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products, 
+        'search_term': query,
     }
 
     return render(request ,'products/products.html', context)
