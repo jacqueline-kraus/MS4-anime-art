4119f4f48c10415e7e232593c78d37e0e371b30a
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-12 13:01:04 +0100

    Add contents to cart contexts

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,7 +1,18 @@
 
 
 def cart_contents(request):
+    cart_items = []
+    total = 0
+    product_count = 0
+    delivery = 4.99
+    grand_total = total + delivery
 
-    context = {}
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
+    }
 
     return context
