fa981719f4e330b8fd93ba9913c0a48dc05c4611
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-16 22:19:34 +0100

    Fix quantity_input buttons javascript logic

--- static/js/quantity_input.js
+++ static/js/quantity_input.js
@@ -7,28 +7,33 @@ function handleEnableDisable(itemId) {
     $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
 }
 
-var allQtyInputs = $('.qty_input');
-for(var i = 0; i < allQtyInputs.length; i++){
-    var itemId = $(allQtyInputs[i]).data('item_id');
-    handleEnableDisable(itemId);
+function updateButtons() {
+    var allQtyInputs = $('.qty_input');
+    for(var i = 0; i < allQtyInputs.length; i++){
+        var itemId = $(allQtyInputs[i]).data('item_id');
+        handleEnableDisable(itemId);
+    }
+
+    $('.qty_input').change(function() {
+        var itemId = $(this).data('item_id');
+        handleEnableDisable(itemId);
+    });
 }
-
-$('.qty_input').change(function() {
-    var itemId = $(this).data('item_id');
-    handleEnableDisable(itemId);
-});
-
 
 $('.increment-qty').click(function(e) {
     e.preventDefault();
     var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
     var currentValue = parseInt($(closestInput).val());
-    $(closestInput).val(currentValue + 1); 
+    $(closestInput).val(currentValue + 1);
+    updateButtons();
 });
 
 $('.decrement-qty').click(function(e) {
     e.preventDefault();
     var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
     var currentValue = parseInt($(closestInput).val());
-    $(closestInput).val(currentValue - 1); 
+    $(closestInput).val(currentValue - 1);
+    updateButtons();
 });
+
+updateButtons();
