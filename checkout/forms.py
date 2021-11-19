7017cb00fa69da2a828be509efb385c79307de55
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-19 22:01:59 +0100

    Add check out form

---
+++ checkout/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Order
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
+
+class CheckoutForm(forms.ModelForm) :
+    class Meta:
+        model = Order
+        fields = ('first_name', 'last_name', 'email',
+                  'street_address', 'postcode', 'city',
+                  'country',)
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'street_address': 'Street Address',
+            'postcode': 'Postal Code',
+            'city': 'City',
+        }
+
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
