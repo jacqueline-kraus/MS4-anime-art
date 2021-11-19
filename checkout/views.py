6addc840c4e2ca23660577d5bcb815faccaca57c
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-19 23:21:37 +0100

    Add view to render checkout with correct form

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import CheckoutForm
 
 
 def checkout(request):
-    return render(request ,'checkout/checkout.html')
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "Your cart is empty. There is nothing to purchase.")
+        return redirect(reverse('products'))
+    
+    order_form = CheckoutForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
