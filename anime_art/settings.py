8572e847661b3e2455ae3bde4161c09b1415f44e
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-19 19:39:31 +0100

    Remove message tags from project setting, as they are built in

--- anime_art/settings.py
+++ anime_art/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from django.contrib.messages import constants as messages
 if os.path.exists('env.py'):
     import env  # noqa
 
@@ -187,13 +186,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MESSAGE_TAGS = {
-    messages.DEBUG: 'alert-secondary',
-    messages.INFO: 'alert-info',
-    messages.SUCCESS: 'alert-success',
-    messages.WARNING: 'alert-warning',
-    messages.ERROR: 'alert-danger',
-}
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
