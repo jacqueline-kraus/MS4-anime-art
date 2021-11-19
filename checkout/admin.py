b47f48395c8019120887877a1b510d780ecf22e1
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-19 22:00:44 +0100

    Add order to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin
+""" Following coding example of Boutique Ado Project """
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order, OrderLineItem
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date','first_name',
+              'last_name', 'email', 'street_address',
+              'city', 'postcode', 'country',
+              'delivery_cost', 'order_total', 'grand_total',)
+    
+    list_display = ('order_number', 'date', 'first_name',
+                    'last_name', 'order_total')
+    
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
