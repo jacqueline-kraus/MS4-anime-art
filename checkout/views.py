a80119a24433edb2499d579292448a010a3d52b8
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-20 19:51:51 +0100

    Add stripe keys

--- checkout/views.py
+++ checkout/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 
 from .forms import CheckoutForm
 
-
 def checkout(request):
     cart = request.session.get('cart', {})
     if not cart:
@@ -13,7 +12,9 @@ def checkout(request):
     order_form = CheckoutForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jo5kuAmaXqFLDCWwC3raun0GhA3Ab1wfoHzV0VH1uIZX0gro8TAM8VJbSI74wc8fObYEP7StLqoSiKX9vNsIK0400IXTWvEyY',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
