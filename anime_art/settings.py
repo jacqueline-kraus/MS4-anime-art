a2e3a85ec364de457cc9bfd527ebc617674de091
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-21 14:52:33 +0100

    Add stripe webhook secret key

--- anime_art/settings.py
+++ anime_art/settings.py
@@ -197,3 +197,4 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
