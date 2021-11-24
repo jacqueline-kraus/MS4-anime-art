1456cf56907b7643175e01173007969ccec870da
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-24 18:50:37 +0100

    Add profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 def profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully!')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+    }
 
     return render(request, template, context)
