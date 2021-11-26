ca83bc7acf8f456203ca7b9467eb4b752b779ca2
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-11-26 01:34:47 +0100

    Change products_list to products view, add delete path

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list , name='products'),
+    path('', views.products, name='products'),
     path('<int:product_id>/', views.product_display , name='product_display'),
-    path('add/', views.add_product , name='add_product'),
+    path('add/', views.add_product, name='add_product'),
     path('update/<int:product_id>/', views.update_product , name='update_product'),
+    path('delete/<int:product_id>/', views.delete_product , name='delete_product'),
 ]
